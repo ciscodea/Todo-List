fbbc10ca82de1263cc2803c8c982abd4738ca13a
Pedro Lopez <pedro.lopez.isw@gmail.com>
2021-03-12 13:30:49 -0600

    first commit

---
+++ app/auth/views.py
@@ -0,0 +1,18 @@
+from flask import render_template,redirect, flash, url_for, session
+from  app.forms import LoginForm
+from . import auth
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    context = {
+        'login_form': login_form
+    }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+        flash('Nombre de usuario registrado con exito!')
+
+        return redirect(url_for('index'))
+    return render_template('login.html', **context)
---
+++ main.py
@@ -0,0 +1,55 @@
+from flask import (
+    request,
+    session, 
+    make_response, 
+    redirect, 
+    render_template,
+    url_for,
+    flash
+)
+
+import unittest
+from app import create_app
+from app.forms import LoginForm
+
+
+app = create_app()
+todos = ['TODO 1', 'TODO 2', 'TODO 3']
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
+
+
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error=error)
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+    response = make_response(redirect('/hello'))
+    session['user_ip'] = user_ip
+
+    return response
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    user_ip = session.get('user_ip')
+    username = session.get('username')
+
+    context = {
+        'user_ip': user_ip,
+        'todos': todos,
+        'username': username
+    }
+    
+    return render_template('hello.html', **context)
